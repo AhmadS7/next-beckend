3e3d868cd6f6e53a3fc6fc47d930204ab33dfb6f
Ahmad <myvirtualrabbit15@gmail.com>
2021-02-02 19:02:50 +0600

    fix typo1sqldelete

--- config/database.js
+++ config/database.js
@@ -24,19 +24,19 @@ module.exports = ({ env }) => {
     };
   }
 
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "sqlite",
-          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
-      },
-    },
-  };
+  // return {
+  //   defaultConnection: "default",
+  //   connections: {
+  //     default: {
+  //       connector: "bookshelf",
+  //       settings: {
+  //         client: "sqlite",
+  //         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+  //       },
+  //       options: {
+  //         useNullAsDefault: true,
+  //       },
+  //     },
+  //   },
+  // };
 };
