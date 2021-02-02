4d115b0fb72a5aac6a7ef5e32ad76265e6b0e40b
Ahmad <myvirtualrabbit15@gmail.com>
2021-02-02 18:48:48 +0600

    fix typo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e27d07f5cd961d658c413c8031048b4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac0ba00c-bc21-45dc-935d-23c6997947e5'
+};
