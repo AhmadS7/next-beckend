062245f80875b41066fdf3ef52d2eccb1966a90f
Ahmad <myvirtualrabbit15@gmail.com>
2021-02-04 14:12:06 +0600

    hellosql

--- config/database.js
+++ config/database.js
@@ -1,42 +1,67 @@
-const parse = require("pg-connection-string").parse;
+// const parse = require("pg-connection-string").parse;
 
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-          },
+// module.exports = ({ env }) => {
+//   if (env("NODE_ENV") === "production") {
+//     const config = parse(process.env.DATABASE_URL);
+//     return {
+//       defaultConnection: "default",
+//       connections: {
+//         default: {
+//           connector: "bookshelf",
+//           settings: {
+//             client: "postgres",
+//             host: config.host,
+//             port: config.port,
+//             database: config.database,
+//             username: config.user,
+//             password: config.password,
+//           },
+//           options: {
+//             ssl: false,
+//           },
+//         },
+//       },
+//     };
+//   }
+
+//   return {
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "bookshelf",
+//         settings: {
+//           client: "sqlite",
+//           filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//         },
+//         options: {
+//           useNullAsDefault: true,
+//         },
+//       },
+//     },
+//   };
+// };
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
         },
       },
-    };
-  }
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "sqlite",
-          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+      options: {
+        ssl: true,
       },
     },
-  };
-};
+  },
+});
